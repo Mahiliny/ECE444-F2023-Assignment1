38589190144c9e7d401678e69240788cb392f8eb
Mahiliny <97846637+Mahiliny@users.noreply.github.com>
2023-09-13 23:36:58 -0400

    Create utils_tests.py
    With tests for the utils functions

---
+++ utils_tests.py
@@ -0,0 +1,16 @@
+from utils import *
+
+#Testing with a string input
+test1 = utils("1234")
+test1.reversed()
+test1.formatter()
+
+#Testing with a float input
+test2 = utils(1234.5)
+test2.reversed()
+test2.formatter()
+
+#Testing with an int input
+test3 = utils(1234)
+test3.reversed()
+test3.formatter()
